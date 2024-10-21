9f29c29746422d64f025e6097b70f7153f6dc69f
Viviane Dias <vivi@mapa.org.br>
2024-10-21 19:31:37 -0300

    feat: switch to NextJs

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Viviane Dias | Portfolio";
-export const SITE_DESCRIPTION = "Welcome to my portfolio!";
