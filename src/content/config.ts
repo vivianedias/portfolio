85beda5140370225e9f6744bafe1650cdb1c5f7e
Viviane Dias <vivi@mapa.org.br>
2024-10-21 14:44:28 -0300

    Initial commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-export const collections = {
-	work: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-		}),
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
 	}),
-};
+});
+
+export const collections = { blog };
