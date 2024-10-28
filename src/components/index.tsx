d9050da6efeca1b2dbc768a87180f0bdfe5d9b44
Viviane Dias <vivianedias@protonmail.com>
2024-10-27 22:07:19 -0300

    feat: adds headline and mini bio

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as ToggleTheme } from "./ToggleTheme";
 export { default as Footer } from "./Footer";
 export { default as CustomMDX } from "./CustomMDX";
+export { default as Sparkle } from "./Sparkle";
